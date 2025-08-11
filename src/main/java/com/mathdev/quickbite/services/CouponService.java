f52fa9cfebe40622f6d3311257fd4081eaffd196
MathDEV-0 <teteuviegas.2006@hotmail.com>
2025-08-11 15:00:19 -0300

    refactor: remove user-coupon relation

--- src/main/java/com/mathdev/quickbite/services/CouponService.java
+++ src/main/java/com/mathdev/quickbite/services/CouponService.java
@@ -8,10 +8,9 @@ import org.springframework.stereotype.Service;
 
 import com.mathdev.quickbite.dto.CouponDTO;
 
-import com.mathdev.quickbite.entities.User;
+
 import com.mathdev.quickbite.repositories.CouponRepository;
 
-import com.mathdev.quickbite.repositories.UserRepository;
 import com.mathdev.quickbite.entities.Coupon;
 
 @Service
@@ -20,8 +19,6 @@ public class CouponService {
 	@Autowired
 	private CouponRepository repo;
 
-	@Autowired
-	private UserRepository userRepository;
 
 	// GET SERVICES
 	public List<CouponDTO> findAll() {
@@ -31,11 +28,6 @@ public class CouponService {
 	public CouponDTO findById(Long id) {
 		Coupon obj = repo.findById(id).orElseThrow(() -> new RuntimeException("Coupon not found!"));
 		return toDTO(obj);
-	}
-
-	public List<CouponDTO> findByClientId(Long userId) {
-		List<Coupon> list = repo.findByClientId(userId);
-		return list.stream().map(this::toDTO).collect(Collectors.toList());
 	}
 
 	// POST SERVICES
@@ -53,11 +45,6 @@ public class CouponService {
 		entity.setDiscount(newData.discount());
 		entity.setExpiry(newData.moment());
 
-		if (newData.clientId() != null) {
-			User user = userRepository.findById(newData.clientId())
-					.orElseThrow(() -> new RuntimeException("User not found"));
-			entity.setClient(user);
-		}
 
 		return toDTO(repo.save(entity));
 	}
@@ -69,8 +56,7 @@ public class CouponService {
 
 	// AUXILIARY METHODS (CONVERSION)
 	private CouponDTO toDTO(Coupon entity) {
-		return new CouponDTO(entity.getId(), entity.getCode(), entity.getDiscount(), entity.getExpiry(),
-				entity.getClient() != null ? entity.getClient().getId() : null);
+		return new CouponDTO(entity.getId(), entity.getCode(), entity.getDiscount(), entity.getExpiry());
 	}
 
 	private Coupon fromDTO(CouponDTO dto) {
@@ -78,12 +64,6 @@ public class CouponService {
 		entity.setCode(dto.code());
 		entity.setDiscount(dto.discount());
 		entity.setExpiry(dto.moment());
-
-		if (dto.clientId() != null) {
-			User user = userRepository.findById(dto.clientId())
-					.orElseThrow(() -> new RuntimeException("User not found"));
-			entity.setClient(user);
-		}
 
 		return entity;
 	}
