08273ec1be88c5a70624f8ec9c84e42c0beb2fc9
MathDEV-0 <teteuviegas.2006@hotmail.com>
2025-08-14 17:29:31 -0300

    refactor:refactoring-current-UserMapper

--- src/main/java/com/mathdev/quickbite/mapper/UserMapper.java
+++ src/main/java/com/mathdev/quickbite/mapper/UserMapper.java
@@ -6,14 +6,21 @@ import com.mathdev.quickbite.entities.User;
 
 public class UserMapper {
 	public static UserDTO toDTO(User entity) {
-		return new UserDTO(entity.getId(),entity.getName(),entity.getEmail(),entity.getAddress());
+		return new UserDTO(entity.getId(), entity.getName(), entity.getEmail(), entity.getAddress());
 	}
+
 	public static User fromDTO(UserDTO dto) {
-		
-        return new User(dto.id(), dto.name(), dto.email(), null, dto.address());
-    }
-	
+
+		return new User(dto.id(), dto.name(), dto.email(), null, dto.address());
+	}
+
 	public static User fromDTOInsert(UserInsertDTO dto) {
-	    return new User(null, dto.name(), dto.email(), dto.password(), dto.address());
+		return new User(null, dto.name(), dto.email(), dto.password(), dto.address());
+	}
+
+	public static void updateData(User entity, UserDTO newUser) {
+		entity.setName(newUser.name());
+		entity.setEmail(newUser.email());
+		entity.setAddress(newUser.address());
 	}
 }
